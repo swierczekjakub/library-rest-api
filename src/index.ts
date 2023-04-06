78c81254eb8be943a1cb0710fb8ee221a0d6de99
swierczekjakub <swierczekjakub1@gmail.com>
2023-04-07 00:04:29 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import * as express from "express"
+import { AppDataSource } from "./data-source"
+
+AppDataSource.initialize().then(async () => {
+    const app = express()
+    app.use(express.json())
+
+    app.listen(3000, () => 'Listening on http://localhost:3000');
+}).catch(error => console.log(error))
