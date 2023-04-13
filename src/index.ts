f4f21b6503c84dd8accb512012fd60fc58962c09
swierczekjakub <swierczekjakub1@gmail.com>
2023-04-13 16:32:28 +0200

    Database connection configured and established

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import * as express from "express"
 import { AppDataSource } from "./data-source"
 
-AppDataSource.initialize().then(async () => {
-    const app = express()
-    app.use(express.json())
+AppDataSource.initialize()
+    .then(async () => {
+        const app = express()
+        app.use(express.json())
 
-    app.listen(3000, () => 'Listening on http://localhost:3000');
-}).catch(error => console.log(error))
+        app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+    })
+    .catch(error => console.log(error))
