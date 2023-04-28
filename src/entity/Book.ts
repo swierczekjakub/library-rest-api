48601db98af6c8a6574ce0674d584686acaa69a2
swierczekjakub <swierczekjakub1@gmail.com>
2023-04-28 14:45:13 +0200

    Added validation via class-validator to Book, Borrowing and Reader entities.

--- src/entity/Book.ts
+++ src/entity/Book.ts
@@ -1,26 +1,46 @@
 import {Entity, Column, OneToMany, PrimaryColumn} from 'typeorm';
 import {Borrowing} from "./Borrowing";
+import {IsDate, IsNotEmpty, IsString, Length, MaxLength} from "class-validator";
 
 @Entity({ name: 'books' })
 export class Book {
+    @IsNotEmpty()
+    @IsString()
+    @Length(36, 36)
     @PrimaryColumn({ type: 'varchar', length: 36, default: () => 'uuid()'})
     id: string;
 
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(255)
     @Column({ type: 'varchar', length: 255 })
     title: string;
 
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(100)
     @Column({ type: 'varchar', length: 100 })
     author: string;
 
+    @IsNotEmpty()
+    @IsString()
+    @Length(10, 17)
     @Column({ type: 'varchar', length: 17 })
     isbn: string;
 
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(255)
     @Column({ type: 'varchar', length: 255 })
     publisher: string;
 
+    @IsNotEmpty()
+    @IsDate()
     @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
     createdAt: Date;
 
+    @IsNotEmpty()
+    @IsDate()
     @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
     updatedAt: Date;
 
--- src/entity/Reader.ts
+++ src/entity/Reader.ts
@@ -1,26 +1,46 @@
 import {Entity, Column, OneToMany, PrimaryColumn} from 'typeorm';
 import {Borrowing} from "./Borrowing";
+import {IsDate, IsNotEmpty, IsPhoneNumber, IsString, Length, MaxLength} from "class-validator";
 
 @Entity({ name: 'readers' })
 export class Reader {
+    @IsNotEmpty()
+    @Length(36, 36)
+    @IsString()
     @PrimaryColumn({ type: 'varchar', length: 36, default: () => 'uuid()'})
     id: string;
 
+    @IsNotEmpty()
+    @MaxLength(50)
+    @IsString()
     @Column({ type: 'varchar', length: 50 })
     firstName: string;
 
+    @IsNotEmpty()
+    @MaxLength(100)
+    @IsString()
     @Column({ type: 'varchar', length: 100 })
     lastName: string;
 
+    @IsNotEmpty()
+    @IsPhoneNumber()
+    @Length(5, 15)
     @Column({ type: 'varchar', length: 15 })
     phoneNumber: string;
 
+    @IsNotEmpty()
+    @MaxLength(255)
+    @IsString()
     @Column({ type: 'varchar', length: 255 })
     email: string;
 
+    @IsNotEmpty()
+    @IsDate()
     @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
     createdAt: Date;
 
+    @IsNotEmpty()
+    @IsDate()
     @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
     updatedAt: Date;
 
