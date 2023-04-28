43ee15d656ac2b8ec94808b5ea0135a9d141e08b
swierczekjakub <swierczekjakub1@gmail.com>
2023-04-28 20:54:43 +0200

    Extended borrowingRepository with findBooksCurrentlyBorrowedByReader and findUnreturnedBook methods.

--- src/repository/BorrowingRepository.ts
+++ src/repository/BorrowingRepository.ts
@@ -1,4 +1,19 @@
 import {AppDataSource} from "../data-source";
 import {Borrowing} from "../entity/Borrowing";
 
-export const borrowingRepository = AppDataSource.getRepository(Borrowing);
+export const borrowingRepository = AppDataSource.getRepository(Borrowing).extend({
+    async findBooksCurrentlyBorrowedByReader(readerId: string) {
+        return this.createQueryBuilder('borrowing')
+            .leftJoinAndSelect('borrowing.book', 'book')
+            .where('borrowing.reader.id = :readerId', {readerId})
+            .andWhere('borrowing.actualReturnDate IS NULL')
+            .getMany();
+    },
+    async findUnreturnedBook(bookId: string) {
+        return this.createQueryBuilder('borrowing')
+            .leftJoinAndSelect('borrowing.book', 'book')
+            .where('book.id = :bookId', {bookId})
+            .andWhere('borrowing.actualReturnDate IS NULL')
+            .getOne();
+    }
+});
